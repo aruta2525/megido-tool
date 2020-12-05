0bd3594598a048da6661571f03d02feb915c929e
t <aruta25252525@gmail.com>
2020-12-05 13:51:59 +0900

    タムス、ベリアルB追加

--- src/ts/data/megido/so/So68.ts
+++ src/ts/data/megido/so/So68.ts
@@ -72,5 +72,76 @@ export const So68: IMegido[] = [
         increment: 0.05
       }
     ]
+  },
+  {
+    clockType: ClockType.So,
+    no: 68,
+    id: 2,
+    name: 'ベリアル',
+    styleType: StyleType.Burst,
+    classType: ClassType.Sniper,
+    hp: 7915,
+    offense: 1096,
+    defense: 628,
+    speed: 381,
+    ability: {name: '巫女の冥助', text: '炎上・狂炎地形がある敵から、受けるダメージを20%軽減する'},
+    skills: [
+      {
+        name: 'アタック',
+        type: SkillType.Atack,
+        levels: [
+          {
+            level: 1,
+            magnification: 1,
+            hit: 1
+          }
+        ]
+      },
+      {
+        name: '焙烙射撃＋',
+        type: SkillType.Skill,
+        levels: [
+          {
+            level: 1,
+            magnification: 1.25,
+            hit: 1
+          }
+        ]
+      },
+      {
+        name: '焙烙射撃＋（Bアーツ）',
+        type: SkillType.Skill,
+        levels: [
+          {
+            level: 1,
+            magnification: 1.4,
+            hit: 1
+          }
+        ]
+      },
+      {
+        name: '不撓の眼光＋',
+        type: SkillType.AwakeningSkill,
+        levels: [
+          {
+            level: 1,
+            magnification: 0,
+            hit: 0
+          }
+        ]
+      },
+      {
+        name: '凶星の煌き＋',
+        type: SkillType.Mysteries,
+        levels: [
+          {
+            level: 1,
+            magnification: 1.35,
+            hit: 5
+          }
+        ],
+        increment: 0.01
+      }
+    ]
   }
 ];
