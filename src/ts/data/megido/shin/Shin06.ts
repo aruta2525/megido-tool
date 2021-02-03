558079f9bb93274367a29730b4d5ce9a21f9cc22
t <aruta25252525@gmail.com>
2021-02-03 11:10:33 +0900

    メギド追加 v1.0.2
    ・フルカス（カウンター）
    ・アラストール（バースト）

--- src/ts/data/megido/shin/Shin06.ts
+++ src/ts/data/megido/shin/Shin06.ts
@@ -58,7 +58,67 @@ const megido: IMegido[] = [
             hit: 1
           }
         ],
-        increment: 0
+        increment: 0.05
+      }
+    ]
+  },
+  {
+    clockType: ClockType.Shin,
+    no: 6,
+    id: 2,
+    name: 'アラストール',
+    styleType: StyleType.Burst,
+    classType: ClassType.Trooper,
+    hp: 8231,
+    offense: 909,
+    defense: 514,
+    speed: 491,
+    ability: {name: 'バタフライエフェクト', text: '毎ターン終了時、覚醒ゲージが+1される'},
+    skills: [
+      {
+        name: 'アタック',
+        type: SkillType.Attack,
+        levels: [
+          {
+            level: 1,
+            magnification: 1,
+            hit: 1
+          }
+        ]
+      },
+      {
+        name: '死のパピリオー＋',
+        type: SkillType.Skill,
+        levels: [
+          {
+            level: 1,
+            magnification: 0,
+            hit: 0
+          }
+        ]
+      },
+      {
+        name: '甘美なる処刑＋',
+        type: SkillType.AwakeningSkill,
+        levels: [
+          {
+            level: 1,
+            magnification: 1,
+            hit: 1
+          }
+        ]
+      },
+      {
+        name: '罪と罰＋',
+        type: SkillType.Mysteries,
+        levels: [
+          {
+            level: 1,
+            magnification: 2.25,
+            hit: 1
+          }
+        ],
+        increment: 0.05
       }
     ]
   }
--- src/ts/data/megido/so/So50.ts
+++ src/ts/data/megido/so/So50.ts
@@ -76,5 +76,68 @@ export const So50: IMegido[] = [
         increment: 0.0166
       }
     ]
+  },
+  {
+    clockType: ClockType.So,
+    no: 50,
+    id: 2,
+    name: 'フルカス',
+    styleType: StyleType.Counter,
+    classType: ClassType.Fighter,
+    hp: 7671,
+    offense: 1136,
+    defense: 441,
+    speed: 479,
+    ability: {
+      name: '恍々惚々',
+      text: '行動開始時、自身が覚醒状態の場合、1ターンの間、自身の攻撃力を25%上昇させバーサーク状態になる(攻撃力最大125%上昇 HP10%ダメージ)'
+    },
+    skills: [
+      {
+        name: 'アタック',
+        type: SkillType.Attack,
+        levels: [
+          {
+            level: 1,
+            magnification: 1,
+            hit: 1
+          }
+        ]
+      },
+      {
+        name: 'アレイジスパイク＋',
+        type: SkillType.Skill,
+        levels: [
+          {
+            level: 1,
+            magnification: 0.9,
+            hit: 2
+          }
+        ]
+      },
+      {
+        name: 'マッドゴーラウンド＋',
+        type: SkillType.AwakeningSkill,
+        levels: [
+          {
+            level: 1,
+            magnification: 0.65,
+            hit: 4
+          }
+        ]
+      },
+      {
+        name: 'クレイジーブルーム＋',
+        type: SkillType.Mysteries,
+        levels: [
+          {
+            level: 1,
+            magnification: 0,
+            hit: 0
+          }
+        ],
+        increment: 0
+      }
+    ]
   }
 ];
